69163cede1e747689c864b9815cfc155dba1c9a9
Marcu Tudor <tudorhome2@gmail.com>
2016-11-19 00:55:52 +0200

    Adaugare suport nume

--- Proiect/php/chat.php
+++ Proiect/php/chat.php
@@ -6,9 +6,16 @@ if($_SERVER["REQUEST_METHOD"] == "GET");
 	{
 		echo "Scriere";
 	    $text = $_GET['text'];
-	    $_SESSION['name'] = "Tudor";
+	    if(isset($_GET['name']))
+	    {
+	    	$_SESSION['name'] = $_GET['name'];
+	    }
+	    else
+	    {
+	    	$_SESSION['name'] = 'Anonim';
+	    }
 	    $fp = fopen("logs/log.txt", 'a');
-	    fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".$_SESSION['name']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
+	    fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".$_SESSION['name']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>\n");
 	    fclose($fp);
 	}
 	//if(isset($_SESSION['text']))
