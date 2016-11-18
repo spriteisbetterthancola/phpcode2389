23acb886b62d0c3d612c3114f2d11763ea260e3d
Marcu Tudor <tudorhome2@gmail.com>
2016-11-19 00:50:57 +0200

    :bug: Rezolvat problema GET

--- Proiect/php/chat.php
+++ Proiect/php/chat.php
@@ -2,23 +2,25 @@
 session_start();
 if($_SERVER["REQUEST_METHOD"] == "GET");
 {
-	if(!isset($_SERVER['text']))
+	if(isset($_GET['text']))
 	{
+		echo "Scriere";
+	    $text = $_GET['text'];
+	    $_SESSION['name'] = "Tudor";
+	    $fp = fopen("logs/log.txt", 'a');
+	    fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".$_SESSION['name']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
+	    fclose($fp);
+	}
+	//if(isset($_SESSION['text']))
+	else
+	{
+		echo "Afisare";
 		$fp = fopen("logs/log.txt", 'r');
 		while (!feof($fp)) {
 			$text = fread($fp, 100);
 			echo $text;
 		}
 		fclose($fp);
-	}
-	//if(isset($_SESSION['text']))
-	else
-	{
-	    $text = $_GET['text'];
-	    $_SESSION['name'] = "Tudor";
-	    $fp = fopen("logs/log.txt", 'a');
-	    fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".$_SESSION['name']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
-	    fclose($fp);
 	}
 }
 //}
