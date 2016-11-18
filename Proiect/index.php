619eb2431cfc4164c5ec5978fc860b05cbd9d56c
spriteisbetterthancola <spriteisbetterthancola@gmx.com>
2016-11-19 00:25:16 +0200

    adaugare cod html

--- Proiect/index.php
+++ Proiect/index.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Hello TI!</title>
+		<title>Chat - Customer Module</title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		
 	</head>
 	<body>
-		<h1>Hello TI!</h1>
-	</body>
+		<div id="wrapper">
+		    <div id="menu">
+		        <p class="welcome">Welcome, <b></b></p>
+		        <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
+		        <div style="clear:both"></div>
+		    </div>
+		     
+		    <div id="chatbox"></div>
+		     
+		    <form name="message" action="">
+		        <input name="usermsg" type="text" id="usermsg" size="63" />
+		        <input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
+		    </form>
+		</div>
+
+</body>
 </html>
