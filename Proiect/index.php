f2924b70eea79dcb1021c43fc8596fe3564634ac
Marcu Tudor <tudorhome2@gmail.com>
2016-11-19 17:56:39 +0200

    :speech_balloon: Chat functional
    MOdiicare chat pentru a functiona

--- Proiect/index.php
+++ Proiect/index.php
@@ -4,10 +4,10 @@
 		<title>Chat - Customer Module</title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<script src="script.js"></script>
+		<script src="javascript/script.js"></script>
 	</head>
 	<script>
-		window.setInterval(refreshFunction(), 2500);//Refresh chat la fiecare 2.5 s
+		window.setInterval(refreshFunction, 2500);//Refresh chat la fiecare 2.5 s
 	</script>
 	<body>
 		<div id="wrapper">
@@ -19,9 +19,9 @@
 		     
 		    <div id="chatbox"></div>
 		     
-		    <form name="message" action="">
-		        <input name="usermsg" type="text" id="usermsg" size="63" />
-		        <input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
+		    <form name="message" action="" onsubmit="return false;">
+		        <input name="text" type="text" id="usermsg" size="63" />
+		        <input name="submitmsg" type="submit"  id="submitmsg" value="Send" onclick="pushMsg()" />
 		    </form>
 		</div>
 
