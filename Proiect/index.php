ef0330206ca19387376721a2db083a36235d6a5f
Marcu Tudor <tudorhome2@gmail.com>
2016-11-19 21:29:20 +0200

    Adaugare input box pentru username

--- Proiect/index.php
+++ Proiect/index.php
@@ -24,7 +24,11 @@
 		     
 		    <div id="chatbox"></div>
 		     
-		    <form name="message" action="" onsubmit="return false;">
+		    <form name="message" action="" onsubmit="return false;" autocomplete="off">
+		    <!-- onsubmit=return false = Pagina nu face refresh la fiecare submit de form 
+				 autocomplete="off" = nu se afiseaza sugestii pentru input 
+				 	(nu se incearca completarea automata a mesajului de trimis)
+		    -->
 		        <input name="text" type="text" id="usermsg" size="63" />
 		        <input name="submitmsg" type="submit"  id="submitmsg" value="Send" onclick="pushMsg()" />
 		    </form>
