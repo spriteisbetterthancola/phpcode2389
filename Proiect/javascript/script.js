228fe2626e026476143a8abda65b618421177aa8
Marcu Tudor <tudorhome2@gmail.com>
2016-11-19 17:57:20 +0200

    Creare functie de Refresh si Send

--- Proiect/javascript/script.js
+++ Proiect/javascript/script.js
@@ -1,9 +1,27 @@
 function refreshFunction()
 {
+	chBox = document.getElementById('chatbox');
+	//chBox.innerHTML = 'Updated';
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("chatbox").innerHTML = this.responseText;
+	    }
+	  };
+	  xhttp.open("GET", "php/chat.php", true);
+	  xhttp.send();
 	return;
 }
-function alertME()
+function pushMsg()
 {
-	alert("Buna!");
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		refreshFunction();
+	};
+	reqString = "php/chat.php?text=";
+	reqString += document.getElementById("usermsg").value;//strip slashes html etc
+	xhttp.open("GET", reqString, true);
+	xhttp.send();
+	document.getElementById("usermsg").value = "";
 }
- 
+
