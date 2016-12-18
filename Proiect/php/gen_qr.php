29273575ce4397583a1ace8cdfc198423fe32610
Marcu Tudor <tudorhome2@gmail.com>
2016-12-18 20:52:21 +0200

    Restructurare
    Restructurare cod. Rezolvare BUG-uri

--- Proiect/php/gen_qr.php
+++ Proiect/php/gen_qr.php
@@ -1,4 +1,5 @@
 <pre>
+<?php require_once "qr_error_correction.php"; ?>
 <?php
 //Generator QR
 if($_SERVER["REQUEST_METHOD"] == "GET")
@@ -7,18 +8,34 @@ if($_SERVER["REQUEST_METHOD"] == "GET")
 	$colums = 3;
 	$M = array_fill(0, $lines, array_fill(0, $colums, 1));
 	$M[1][1] = 0;
-	echo ascii_print($M);
+	//echo ascii_print($M);
 	//$var = ascii_print($M);
-	
+	//echo qr_log(0);
 	if(isset($_GET["text"]))
 	{
 		generate_qr($_GET["text"]);
 	}
+	///* DEBUG STUFFS
+	/*
 
+	$p7 = array(qr_pow(0), 1);//1
+	$p7 = multiply_polynoms($p7, array(qr_pow(1), 1));//2
+	$p7 = multiply_polynoms($p7, array(qr_pow(2), 1));//3
+	$p7 = multiply_polynoms($p7, array(qr_pow(3), 1));//4
+	print_polin($p7);
+	a1x4 + a15x3 + a54x2 + a120x1 + a64
+	$p7 = multiply_polynoms($p7, array(qr_pow(4), 1));//5
+	$p7 = multiply_polynoms($p7, array(qr_pow(5), 1));//6
+	$p7 = multiply_polynoms($p7, array(qr_pow(6), 1));//7
+	$p7 = multiply_polynoms($p7, array(qr_pow(7), 1));//8
+	$p7 = multiply_polynoms($p7, array(qr_pow(8), 1));//9
+	$alpha = qr_alpha_pol($p7);
+	print_polin($alpha);
+	//print_polin(qr_alpha_pol(multiply_polynoms($p7, array(0,1))));
 	
+	//*/
 }
 ?>
-
 <?php
 
 function generate_qr($text)
@@ -30,7 +47,7 @@ function generate_qr($text)
 			//Vom alege Vesiunea 4 cu Error Corection Quality
 	$qr_version = 4;
 	$qr_mode = "alphanumeric";
-	$qr_error_correction = "H";
+	$qr_error_correction_level = "H";
 	$qr_caracter_capacity_table = array(1 => array('L' => array('numeric' => 41, 'alphanumeric' => 25, 'byte' => 17),
 												   'M' => array('numeric' => 34, 'alphanumeric' => 20, 'byte' => 14),
 												   'Q' => array('numeric' => 27, 'alphanumeric' => 16, 'byte' => 11),
@@ -77,12 +94,11 @@ function generate_qr($text)
 	while (strlen($encoded_data) % 8 != 0) {
 		$encoded_data = $encoded_data . "0";
 	}
-	var_dump($encoded_data);
 
 	if(strlen($encoded_data) < $bits_required)
 	{
 		$pad_grups = intdiv_1(($bits_required - strlen($encoded_data)), 8);
-		var_dump($pad_grups);
+		//var_dump($pad_grups);
 		$pad_strings = array(0 => "11101100", 1 => "00010001");
 		for($i = 0; $i < $pad_grups; $i++)
 		{
@@ -90,9 +106,14 @@ function generate_qr($text)
 		}
 	}
 	
+	//var_dump($encoded_data);
+	
 	//3 - Calculare cod detectie de erori CRC
 		//4-H desparte codul in o grupa cu 4 blocuri a cate 9 cuvinte de cod fiecare
 		//Un cuvant de cod are 8 biti din mesajul codat
+	$qr_code_blocks = qr_split_encoded_data($encoded_data, $qr_version, $qr_error_correction_level);
+		//var_dump($qr_code_blocks);//afisare blocuri
+	$qr_ec_blocks = qr_gen_ec_blocks($qr_code_blocks, $qr_version, $qr_error_correction_level);
 }
 
 function intdiv_1($a, $b){
@@ -107,11 +128,11 @@ function to_binary_string($value)//Nu e folosita!
 function alfa_encode($text)
 {
 	$qr_alphanum_table = array(
-			"0" => 0, "1" => 1, "2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8, "9" => 9, 
-			"A" => 10, "B" => 11, "C" => 12, "D" => 13, "E" => 14, "F" => 15, "G" => 16, "H" => 17, "I" => 18, 
-			"J" => 19, "K" => 20, "L" => 21, "M" => 22, "N" => 23, "O" => 24, "P" => 25, "Q" => 26, "R" => 27, 
-			"S" => 28, "T" => 29, "U" => 30, "V" => 31, "W" => 32, "X" => 33, "Y" => 34, "Z" => 35, " " => 36, 
-			"$" => 37, "%" => 38, "*" => 39, "+" => 40, "-" => 41, "." => 42, "/" => 43, ":" => 44);
+			"0" =>  0, "1" =>  1, "2" =>  2, "3" =>  3, "4" =>  4, "5" =>  5, "6" =>  6, "7" =>  7, "8" => 8, 
+			"9" =>  9, "A" => 10, "B" => 11, "C" => 12, "D" => 13, "E" => 14, "F" => 15, "G" => 16, "H" => 17, 
+			"I" => 18, "J" => 19, "K" => 20, "L" => 21, "M" => 22, "N" => 23, "O" => 24, "P" => 25, "Q" => 26, 
+			"R" => 27, "S" => 28, "T" => 29, "U" => 30, "V" => 31, "W" => 32, "X" => 33, "Y" => 34, "Z" => 35, 
+			" " => 36, "$" => 37, "%" => 38, "*" => 39, "+" => 40, "-" => 41, "." => 42, "/" => 43, ":" => 44);
 	$output = "";
 	for ($i=0, $tl = strlen($text); $i < $tl; $i = $i + 2) { 
 		$block = $qr_alphanum_table[$text[$i]];
@@ -157,6 +178,8 @@ function ascii_print($matrix)
 	$output .= "\n</pre>";
 	return $output;
 }
+
+
 ?>
 
 </pre>
