f2438ea153878e14715c07e4b7c7be8daa8673ae
spriteisbetterthancola <spriteisbetterthancola@gmx.com>
2017-01-07 18:29:49 +0200

    Adaugare pagina home Prima pagina a siteului

---
+++ Proiect/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+	<head>
+		<title> Home </title>
+		<meta charset="UTF-8">
+		<link  type ="text/css" rel ="stylesheet" href="css/style.css"/>
+	</head>
+	<body>
+		<section id = "S1">
+			<div  class="button" id="new_button">NEW</div>
+			<div  class ="button" id = "join_button">JOIN</div>
+			<input type="text" name= "ceva">
+		</section>
+	</body>
+
+</html>
