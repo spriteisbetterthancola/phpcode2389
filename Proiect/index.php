3c5d5352a3b7cc086c31b9d2e8240a73df3c8475
spriteisbetterthancola <spriteisbetterthancola@gmx.com>
2017-01-09 01:49:00 +0200

    Fostul home2.html

--- Proiect/index.php
+++ Proiect/index.php
@@ -1,38 +1,84 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Chat - Customer Module</title>
-		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<script src="javascript/script.js"></script>
+		<title> Home </title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="css/w3.css">
+		<script type="text/javascript" src="javascript/script.js"></script>
+
 	</head>
-	<script>
-		window.setInterval(refreshFunction, 2500);//Refresh chat la fiecare 2.5 s
-	</script>
-	<body>
-		<div id="wrapper">
-		    <div id="menu">
-		        <p class="welcome">Welcome, 
-		        	<p id="username">
-		        		<input type="text" name="name" id="userID" onblur="inputTextToDiv(this)">
-		        	</p>
-		        </p>
+	<body class="w3-display-container">
+	<header class="w3-container w3-teal w3-padding w3-center">
+		<h1 class ="w3-animate-top w3-xxxlarge">Meeting Pit</h1>
+	</header>
+	<section id="main-wrapper" class="w3-row-padding w3-center w3-margin-top">
+		<section id="join-wrapper" class="w3-half">
+			<section id="join-card" class="w3-card-2" style="min-height:469px">
+				<head id="join-head">
+				  <h3 class="w3-teal w3-padding-top">Join</h3>
+				    <section id="join-form-wrapper" class="w3-row-padding w3-center">
+					  <form class="w3-panel" style="border: 0px solid !important;"  action="pit.php" 
+					  onsubmit="return js_validate(this);" method="post" name ="join_form">
+								<div class="w3-group">
+									<input class="w3-input " type ="text" required="required" name="idConversatie"  id="idConversatie"
+									onchange="js_validate(this)" id="idConversatie">
+									<label class="w3-label w3-validate">Id conversație</label>
+								</div>
 
-		        <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
-		        <div style="clear:both"></div>
-		    </div>
-		     
-		    <div id="chatbox"></div>
-		     
-		    <form name="message" action="" onsubmit="return false;" autocomplete="off">
-		    <!-- onsubmit=return false = Pagina nu face refresh la fiecare submit de form 
-				 autocomplete="off" = nu se afiseaza sugestii pentru input 
-				 	(nu se incearca completarea automata a mesajului de trimis)
-		    -->
-		        <input name="text" type="text" id="usermsg" size="63" />
-		        <input name="submitmsg" type="submit"  id="submitmsg" value="Send" onclick="pushMsg()" />
-		    </form>
-		</div>
+								<div class="w3-group">
+									<input class="w3-input" type ="text" required="required"  name ="nickName" id="nickName"
+									onchange="js_validate(this)" id= "nickname">
+									<label class="w3-label w3-validate">Utilizator</label>
+								</div>
 
-</body>
+								<div class="w3-group">
+									<input type="hidden" name="isValidJoin" id="isValidJoin" value="0"/>
+									<input type="submit" class="w3-btn" name="join" id="join" value="Join"/>
+								</div>	
+					  </form>
+					</section>
+				</head>
+			</section>
+		</section>
+		<section id="new-wrapper" class="w3-half">
+			<section id="new-card" class="w3-card-2" style="min-height:469px">
+			<head id="join-head">
+					<h3 class="w3-teal w3-padding-top">New Conversation</h3>
+					<section id="join-form-wrapper" class="w3-row-padding w3-center">
+					  <form class="w3-panel" style="border: 0px solid !important;"  action="pagina.php" 
+					  onsubmit="return js_validate_new(this);" method="post">
+								<div class="w3-group">
+									<input class="w3-input" type ="text" required="required" name ="conversationName" 
+									onchange="js_validate_new(this)" id ="conversationName">
+									<label class="w3-label w3-validate">Nume conversație</label>
+								</div>
+
+								<div class="w3-group">
+									<input class="w3-input" type="text" required="required"  name="adminName" 
+									onchange="js_validate_new(this)" id="adminName">
+									<label class="w3-label w3-validate">Nume Administrator</label>
+								</div>
+
+								<div class="w3-group">
+									<input class="w3-input" type ="text" required="required"  name="pswd" 
+									onchange="js_validate_new(this)" id="passwordInput">
+									<label class="w3-label w3-validate">Parola</label>
+								</div>
+
+								<div class="w3-group">
+									<input type="hidden" name="isValidJoin" id="isValidNew" value="0"/>
+									<input type="submit" class="w3-btn" name="newConvBtn" id="newConvBtn" value="Create"/>
+								</div>	
+					  </form>
+					  </section>
+				</head>
+			</section>
+		</section>
+	</section>
+
+
+	<footer class="w3-row w3-theme w3-padding w3-center w3-margin-top" >
+	&copy;TI PROIECT 2017
+	</footer>
+	</body>
 </html>
