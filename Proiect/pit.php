e4d6508a9807100f176f61c20a0fb1f3d2b98a9c
Marcu Tudor <tudorhome2@gmail.com>
2017-01-09 01:06:01 +0200

    :dart: Adaugare cod demonstrativ pentru mesaje

--- Proiect/pit.php
+++ Proiect/pit.php
@@ -23,7 +23,6 @@ if($_SERVER['REQUEST_METHOD'] == "POST")//Provine din home.php
 	}
 }
 
-<<<<<<< HEAD
 $conv_uid = 0;
 $usr_name = 0;
 
@@ -31,14 +30,6 @@ $usr_name = 0;
 <html>
 	<head>
 		<meta charset="UTF-8">
-=======
-//DEBUG ONLY!!!
-$usr_name = "Ana";
-$conv_uid = "";
-?>
-<html>
-	<head>
->>>>>>> e65f751bacdaa29d8c02ba0c892e28938413f6b6
 		<link rel="stylesheet" type="text/css" href="css/w3.css">
 	</head>
 	<body>
@@ -48,22 +39,59 @@ $conv_uid = "";
 	</form>
 		<main> <section>
 			<header class="w3-container w3-theme w3-padding w3-center">
-				<h1 class ="w3-animate-bottom w3-xxxlarge">Lowerthyi ghhui</h1>
+				<h1 class ="w3-animate-top w3-xxxlarge">Conversation Title</h1>
 			</header>
+<section class ="w3-container" id="msg-box">
 
-			<section class =" w3-container">
-				<section class ="w3-card-2 w3-padding w3-group" style="height:450px;">
-				<div class="message">
-					<p class="msg-body">
-				</div>
-					<p style="color: blue;text-align: left;display: left">Hello<br/><i>12:12:22&nbsp; Admin:</i></p>
-					<p style="color: green;text-align:right"> Salut<br/> <i> 1:23:09 &nbsp; Florin:</i></p>
-				</section>
-			</section>
-		</section></main>
+
+
+  <section class ="w3-container w3-card-4 w3-padding-top" style="height:450px;">
+
+<section class="w3-container">
+  <section class="w3-row w3-left w3-padding-0 ">
+    <section class="w3-right w3-small w3-text-red"><b>Admin</b></section>
+    <section class="w3-left w3-padding-right w3-tiny"><i>23:59:59</i></section>
+  </section><br/>
+  <section class="w3-row w3-left w3-padding-0 w3-margin-0">
+  	<article class="w3-padding w3-amber">Hello World</article>
+  </section>
+</section><br/>
+
+<section class="w3-container">
+  <section class="w3-row w3-left w3-padding-0 ">
+    <section class="w3-right w3-small"><b>UserName</b></section>
+    <section class="w3-left w3-padding-right w3-tiny"><i>23:59:59</i></section>
+  </section><br/>
+  <section class="w3-row w3-left w3-padding-0 w3-margin-0">
+  	<article class="w3-padding w3-pale-green">Hello World</article>
+  </section>
+</section><br/>
+
+<section class="w3-container">
+  <section class="w3-row w3-right w3-padding-0 ">
+    <section class="w3-right w3-tiny"><i>23:59:59</i></section>
+  </section><br/>
+  <section class="w3-row w3-right w3-padding-0 w3-margin-0">
+  	<article class="w3-padding w3-pale-blue">Hello World</article>
+  </section><br/>
+
+  
+</section><br/>
+</section>
+
+<section id="send-box" class="w3-panel">
+    <form class="w3-panel w3-row w3-display-container" action="" onsubmit="false;">
+    	<input type="text" class="w3-input w3-threequarter w3-display-left" name="msg_text"/>
+    	<input type="submit" class="w3-input w3-quarter w3-display-right" name="send-btn" value="SEND"/>
+    </form>
+</section>
+
+
+  </main>
+  <!--
 		<footer class="w3-container w3-theme w3-padding w3-center">
 			&copy;TI PROIECT 2017
 		</footer>
-	
+	-->
 	</body>
 </html>
